c630e7e2840b0518d55dca5524cedbf14666fbef
Yufeng Liu Reach <yufeng8@vt.edu>
2017-11-11 15:04:28 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,12 +1,12 @@
 function forLoop(array) {
-    for (var i = 0; i < 25; i ++) {
-      if (i === 1) {
-         array.push("I am 1 strange loop."));
-      } else {
-         array.push(`I am ${i} strange loops.`));
-      }
+  for (var i = 0; i < 25; i ++) {
+    if (i === 1) {
+       array.push("I am 1 strange loop.");
+    } else {
+       array.push(`I am ${i} strange loops.`);
     }
-    return array
+  }
+  return array
 }
 
 /*
