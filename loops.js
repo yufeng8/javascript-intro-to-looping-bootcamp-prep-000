596375e2b63b1621992c03182be1a9defe4d7c19
Yufeng Liu Reach <yufeng8@vt.edu>
2017-11-11 15:00:52 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -3,7 +3,7 @@ function forLoop(array) {
       if (i === 1) {
          array.push("I am 1 strange loop."));
       } else {
-         array.push("I am ${i} strange loops."));
+         array.push(`I am ${i} strange loops.`));
       }
       return array
     }
